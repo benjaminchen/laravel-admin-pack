94ff7173b11be45c1327ec100b98543e517c7df5
benjamin <benjaminchen.c@gmail.com>
2017-02-10 15:10:12 +0800

    update version info in blade

--- src/views/components/footer.blade.php
+++ src/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-lg-10 col-lg-offset-2 col-md-10 col-md-offset-2 main-footer">
         <div class="pull-right hidden-xs">
-            <strong>{{ trans('adminPack::admin.version') }}</strong>&nbsp;&nbsp; 1.0
+            <strong>{{ trans('adminPack::admin.version') }}</strong>&nbsp;&nbsp; 0.1.1
         </div>
         <strong>Powered by 
             <a href="https://github.com/benjaminchen/laravel-admin-pack" target="_blank">Laravel-admin-package</a>
